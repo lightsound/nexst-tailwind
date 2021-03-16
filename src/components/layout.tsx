98ec4629b2d76deaf7f8c39e239eeb3c071c50a8
skoto <0120koto@gmail.com>
2021-03-16 11:15:11 +0900

    🐛

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,9 +4,9 @@ import { Header } from "src/components/header";
 
 export const Layout: VFC<{ children: ReactNode }> = (props) => {
   return (
-    <div className="min-h-screen grid grid-rows-[auto,1fr,auto]">
+    <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="bg-red-300">{props.children}</main>
+      <main className="flex-1 bg-red-300">{props.children}</main>
       <Footer />
     </div>
   );
