a0330d6662b93267755ed1c11ac74a66b6c67240
skoto <0120koto@gmail.com>
2021-03-16 10:58:07 +0900

    🔥

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: ["./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "media", // 'media' or 'class'
-  theme: { extend: { colors: { "accent-1": "#333" } } },
+  darkMode: false, // 'media' or 'class'
+  theme: { extend: {} },
   variants: { extend: {} },
   plugins: [],
 };
