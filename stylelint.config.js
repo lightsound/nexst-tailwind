e5814dc6c1b5d3dacdd80764e5560d2c476432cc
skoto <0120koto@gmail.com>
2021-04-13 12:47:43 +0900

    ➖ Remove stylelint.

--- stylelint.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ["stylelint-config-standard"],
-  rules: {
-    "at-rule-no-unknown": [true, { ignoreAtRules: ["tailwind", "apply", "variants", "responsive", "screen"] }],
-    "comment-empty-line-before": null,
-  },
-};
