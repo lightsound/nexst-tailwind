d624d86c0c9f36a567e81839de70855eec062ec6
skoto <0120koto@gmail.com>
2021-05-10 18:51:50 +0900

    🎨 Use next.config.js props for i18n.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   future: { webpack5: true, strictPostcssConfiguration: true },
+  i18n: { locales: ["ja"], defaultLocale: "ja" },
   reactStrictMode: true,
   typescript: { ignoreDevErrors: true },
   poweredByHeader: false,
