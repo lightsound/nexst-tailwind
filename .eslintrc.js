86c841ad8b02bf01034575c05b31a23296151947
skoto <0120koto@gmail.com>
2021-07-16 13:10:59 +0900

    🔧 Update ESLint config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -46,17 +46,29 @@ module.exports = {
     "@typescript-eslint/no-var-requires": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/consistent-type-imports": ["warn", { prefer: "type-imports" }],
-    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-unused-vars": ["error", { varsIgnorePattern: "^_", argsIgnorePattern: "^_" }],
     "@typescript-eslint/naming-convention": [
       "error",
       { selector: ["typeAlias", "typeParameter"], format: ["PascalCase"] },
-      { selector: ["property", "parameterProperty", "method"], format: ["camelCase"] },
-      { selector: "variable", types: ["boolean"], format: ["PascalCase"], prefix: ["is", "has", "should"] },
+      { selector: ["property", "method"], format: ["camelCase"] },
+      {
+        selector: "variable",
+        types: ["boolean"],
+        format: ["PascalCase"],
+        prefix: ["no", "is", "has", "should"],
+        filter: { regex: "^_", match: false },
+      },
     ],
+    "jsx-a11y/no-autofocus": "off",
     "jsx-a11y/anchor-is-valid": [
       "error",
       { components: ["Link"], specialLink: ["hrefLeft", "hrefRight"], aspects: ["invalidHref", "preferButton"] },
     ],
   },
-  overrides: [{ files: ["src/pages/**/*.tsx"], rules: { "import/no-default-export": "off" } }],
+  overrides: [
+    {
+      files: ["src/pages/**/*.tsx", "src/pages/api/**/*.ts"],
+      rules: { "import/no-default-export": "off" },
+    },
+  ],
 };
