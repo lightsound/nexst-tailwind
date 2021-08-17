38ec5b3d563aa80617beef289084caa5ae1e9997
skoto <0120koto@gmail.com>
2021-08-17 21:16:47 +0900

    🏷️ next.config.jsに型追加

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
+// @ts-check
+
 /**
- * @type {import('next/dist/next-server/server/config').NextConfig}
- */
+ * @type {import('next').NextConfig}
+ **/
 const nextConfig = {
   rewrites: async () => {
     return [{ source: "/", destination: "/root" }];
   },
   i18n: { locales: ["ja"], defaultLocale: "ja" },
-  future: { strictPostcssConfiguratio: true },
+  future: { strictPostcssConfiguration: true },
   pageExtensions: ["page.tsx", "page.ts"],
   reactStrictMode: true,
   typescript: { ignoreDevErrors: true },
