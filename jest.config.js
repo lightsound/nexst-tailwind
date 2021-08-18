14dc723ccfb99749b08aab32c1362c0025f92a92
skoto <0120koto@gmail.com>
2021-08-18 14:46:10 +0900

    🚚 ディレクトリ構造変更

--- jest.config.js
+++ jest.config.js
@@ -10,9 +10,9 @@ module.exports = {
     // Handle CSS imports (with CSS modules): https://jestjs.io/docs/webpack#mocking-css-modules
     "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
     // Handle CSS imports (without CSS modules)
-    "^.+\\.(css|sass|scss)$": "<rootDir>/__jest__/config/mockStyle.js",
+    "^.+\\.(css|sass|scss)$": "<rootDir>/jest/mockStyle.js",
     // Handle image imports: https://jestjs.io/docs/webpack#handling-static-assets
-    "^.+\\.(jpg|jpeg|png|gif|webp|svg)$": `<rootDir>/__jest__/config/mockFile.js`,
+    "^.+\\.(jpg|jpeg|png|gif|webp|svg)$": `<rootDir>/jest/mockFile.js`,
     // Absolute Imports and Module Path Aliases
     "src/(.*)": "<rootDir>/src/$1",
   },
@@ -22,6 +22,6 @@ module.exports = {
     "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
   },
   transformIgnorePatterns: ["/node_modules/", "^.+\\.module\\.(css|sass|scss)$"],
-  setupFilesAfterEnv: ["<rootDir>/__jest__/config/jest.setup.js"],
-  snapshotResolver: "<rootDir>/__jest__/config/jest.snapshot.js",
+  setupFilesAfterEnv: ["<rootDir>/jest/jest.setup.js"],
+  snapshotResolver: "<rootDir>/jest/jest.snapshot.js",
 };
