27ac8a1e9203ac4019d151823bd4fd84fc6c2b49
skoto <0120koto@gmail.com>
2021-12-11 16:44:24 +0900

    ⬆️ Upgrade Tailwind CSS v3.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./pages/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
-  darkMode: false, // 'media' or 'class'
+  content: ["./pages/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
   theme: { extend: {} },
-  variants: { extend: {} },
   plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography"), require("@tailwindcss/line-clamp")],
 };
